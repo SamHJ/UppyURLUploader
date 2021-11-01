56fb2933cc21626ccb1a95b07363ac18bcd6cd37
samhj <sam4gentle@gmail.com>
2021-11-01 14:17:39 +0100

    update

--- client/assets/uppy.js
+++ client/assets/uppy.js
@@ -2,38 +2,63 @@ import("https://releases.transloadit.com/uppy/v2.2.1/uppy.min.js")
 
 let fileInfo = {}
 
+let isFromURL = false
+
+document.addEventListener("DOMContentLoaded", function(){
+  const urlBtn = document.querySelector('[data-uppy-acquirer-id="Url"]')
+  const fileBtn = document.querySelector('[data-uppy-acquirer-id="MyDevice"]')
+  urlBtn.addEventListener('click',(e)=>isFromURL = true)
+  fileBtn.addEventListener('click',(e)=>isFromURL=false)
+});
+
 const uppy = new Uppy.Core({
     debug: true, 
     autoProceed: false,
     onBeforeFileAdded: (currentFile, _) => {
-        fileInfo = {
-            name: currentFile.name,
-            type: currentFile.type,
-            extension: currentFile.extension,
-            size: currentFile.data.size
-        }
-
-        uppy.use(Uppy.Tus, { headers: {fileInfo:JSON.stringify(fileInfo)}, endpoint: 'http://localhost:3020/upload/' })
+      // if(isFromURL){
+      //     fileInfo = {
+      //         name: currentFile.name,
+      //         type: currentFile.type,
+      //         extension: currentFile.extension,
+      //         size: currentFile.data.size
+      //     }
+      //     const headers = {fileInfo:JSON.stringify(fileInfo)}
+      //     uppy.use(Uppy.Tus, { headers, endpoint: 'http://localhost:3020/upload/' })
+      //   }else{
+          uppy.use(Uppy.XHRUpload, {endpoint: 'http://localhost:3020/xhr-upload', fieldName: 'file', formData: true })
+        // }
     }
   })
-  .use(Uppy.Dashboard, { inline: true, target: '#drag-drop-area',showProgressDetails: true, })
+  .use(Uppy.Dashboard, { inline: true, target: '#drag-drop-area',showProgressDetails: true})
   .use(Uppy.Url, { target: Uppy.Dashboard, companionUrl: 'http://localhost:3020' })
   .use(Uppy.GoldenRetriever)
   
-  uppy.on('complete', result => {
-    if(result.successful){
-        document.querySelector('.uppy-file-url-input').value = result.successful[0].uploadURL.split("/upload/").join("/")
+  uppy.on('complete', res => {
+    if(res.successful){
+      const item = res.successful[0]
+      if(item){
+        let fileURL = ''
+        if(isFromURL){
+          console.log('from url: ')
+          fileURL = item.uploadURL.split("/upload/").join("/")
+        }else{
+          console.log('from formdata: ')
+          fileURL = `https://dload.tapnaija.com/uploads/${item.name}`
+        }
+
+        document.querySelector('.uppy-file-url-input').value = fileURL
+      }
     }
-    flushTUS()
+    flushPlugin()
   })
 
   uppy.on('upload-error', (file, error, response) => {
-    flushTUS()
+    flushPlugin()
   });
 
-  const flushTUS = ()=>{
-    //remove Tus plugin from uppy instnce
-    uppy.removePlugin(uppy.getPlugin('Tus'))
+  const flushPlugin = ()=>{
+    if(isFromURL) return uppy.removePlugin(uppy.getPlugin('Tus'));
+    uppy.removePlugin(uppy.getPlugin('XHRUpload'))
   }
 
   const copyToClipboard = ()=>{
--- server/index.js
+++ server/index.js
@@ -1,36 +1,52 @@
 const express = require('express')
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const companion = require('@uppy/companion')
 const tus = require('tus-node-server');
+const path = require('path');
+const multer = require('multer')
 const server = new tus.Server();
 const uploadApp = express();
 
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "../", "downloads"),
+  filename: (req,file,cb)=>{
+    cb(null,file.originalname)
+  }
+})
+
+const uploadFile = multer({storage}).single('file')
+
+const getFileName = (req)=> {
+  const fileinfo = JSON.parse(req.headers.fileinfo)
+  if(fileinfo.extension) return fileinfo.name
+
+  /*handle the case where this file's url has no extension attached to it, 
+    but leads to a downloadable content
+    e.g https://avatars.githubusercontent.com/u/8690438?s=88&u=417f4a3aae31bbc99de48c827523cffcb1ec845e&v=4
+  */
+  const newName = +new Date()
+  let extension = 'txt' //default file extension
+
+  const mE = fileinfo.type.split("/")[1] /* most likely extension. e.g image/jpeg returns jpeg 
+  as the most likely extension. For application/pdf it is pdf, for image/gif, it is gif etc.
+  */
+  if(mE){
+    extension = mE
+  }
+  return `${newName}.${extension}`
+}
+
 server.datastore = new tus.FileStore({
   path: '/downloads',
-  namingFunction: (req)=> {
-    const fileinfo = JSON.parse(req.headers.fileinfo)
-    if(fileinfo.extension) return fileinfo.name
-
-    /*handle the case where this file's url has no extension attached to it, 
-      but leads to a downloadable content
-      e.g https://avatars.githubusercontent.com/u/8690438?s=88&u=417f4a3aae31bbc99de48c827523cffcb1ec845e&v=4
-    */
-    const newName = +new Date()
-    let extension = 'txt' //default file extension
-
-    const mE = fileinfo.type.split("/")[1] /* most likely extension. e.g image/jpeg returns jpeg 
-    as the most likely extension. For application/pdf it is pdf, for image/gif, it is gif etc.
-    */
-    if(mE){
-      extension = mE
-    }
-    return `${newName}.${extension}`
-  }
+  namingFunction: (req)=>getFileName(req)
 });
 
 const app = express()
 
+app.use(cors())
+app.options('*', cors())
 app.use(bodyParser.json())
 app.use(session({
   secret: 'my_uppy_secret',
@@ -46,6 +62,12 @@ app.use((req, res, next) => {
 uploadApp.all('*', server.handle.bind(server));
 
 app.use('/upload',uploadApp);
+
+app.post('/xhr-upload',uploadFile,(req,res)=>{
+  if(req.file) return res.json({success:true})
+
+  res.send("file upload failed")
+})
 
 // initialize uppy
 const uppyOptions = {
